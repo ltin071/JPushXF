f31e0b490de70a37547b2419729e9021352c3d07
sleungdtt <sleung@deloitte.com.hk>
2019-03-19 15:33:03 +0800

    initial commit

---
+++ JPush/JPush.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using CN.Jpush.Android.Api;
+using Android.Content;
+
+namespace JPush.Droid
+{
+    [Activity(Label = "JPush", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            initPushNotification();
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        private void initPushNotification()
+        {
+            IntentFilter filter = new IntentFilter();
+            filter.AddAction(JPushInterface.ActionNotificationOpened);
+            filter.AddAction(JPushInterface.ActionNotificationReceived);
+            filter.AddAction(JPushInterface.ActionMessageReceived);
+            filter.AddAction(JPushInterface.ActionRegistrationId);
+            filter.AddAction(JPushInterface.ActionConnectionChange);
+            NotificationReceiver receiver = new NotificationReceiver();
+            RegisterReceiver(receiver, filter);
+            JPushInterface.SetDebugMode(true);
+            JPushInterface.Init(this.ApplicationContext);
+        }
+    }
+}
---
+++ JPush/JPush.Android/NotificationReceiver.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using CN.Jpush.Android.Api;
+using CN.Jpush.Android.Service;
+using Plugin.LocalNotifications;
+
+namespace JPush.Droid
+{
+    [BroadcastReceiver]
+    [IntentFilter(new string[] { "cn.jpush.android.intent.REGISTRATION" }, Categories = new string[] { "com.companyname.JPush" })]
+    [IntentFilter(new string[] { "cn.jpush.android.intent.MESSAGE_RECEIVED" }, Categories = new string[] { "com.companyname.JPush" })]
+    [IntentFilter(new string[] { "cn.jpush.android.intent.NOTIFICATION_RECEIVED" }, Categories = new string[] { "com.companyname.JPush" })]
+    [IntentFilter(new string[] { "cn.jpush.android.intent.NOTIFICATION_OPENED" }, Categories = new string[] { "com.companyname.JPush" })]
+    [IntentFilter(new string[] { "cn.jpush.android.intent.CONNECTION" }, Categories = new string[] { "com.companyname.JPush" })]
+    class NotificationReceiver : PushReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            base.OnReceive(context, intent);
+            if (intent.Action == JPushInterface.ActionNotificationReceived)
+            {
+                //When user tap the notification on notification center
+                Bundle bundle = intent.Extras;
+                string notificationData = bundle.GetString(JPushInterface.ExtraAlert);
+                CrossLocalNotifications.Current.Show("JPush", notificationData, 101);
+            }
+            if (intent.Action == JPushInterface.ActionRegistrationId)
+            {
+                //Only call when first launch, get the registrationID
+                string regID = JPushInterface.GetRegistrationID(context);
+            }
+
+        }
+    }
+}
